9ff9732d14c76dfee2410cac238d69ac4a68cb38
spatel354 <sunnypatel18@gatech.edu>
2015-03-05 04:20:39 -0500

    wishlist submit

--- app/src/main/java/yesmen/cs2340/shoppingwithfriends/Wishlist.java
+++ app/src/main/java/yesmen/cs2340/shoppingwithfriends/Wishlist.java
@@ -2,12 +2,14 @@ package yesmen.cs2340.shoppingwithfriends;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -16,24 +18,37 @@ import java.util.ArrayList;
  */
 public class Wishlist extends Activity implements View.OnClickListener {
 
-        private Button cancelButton,submitButton;
-        private EditText enteredItem, enteredPrice;
-        private User userObject;
+    private Button cancelButton, submitButton;
+    private EditText enteredItem, enteredPrice;
+    private ProgressDialog progressDialog;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
 
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.wishlist);
-            submitButton = (Button) findViewById(R.id.wishlist_submit_button);
-            submitButton.setOnClickListener(this);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_wishlist);
+        submitButton = (Button) findViewById(R.id.wishlist_submit_button);
+        submitButton.setOnClickListener(this);
 
-            cancelButton = (Button) findViewById(R.id.wishlist_cancel_button);
-            cancelButton.setOnClickListener(this);
+        cancelButton = (Button) findViewById(R.id.wishlist_cancel_button);
+        cancelButton.setOnClickListener(this);
 
-            enteredItem = (EditText) findViewById(R.id.my_item_input);
-            enteredPrice = (EditText) findViewById(R.id.my_price_input);
+        enteredItem = (EditText) findViewById(R.id.my_item_input);
+        enteredPrice = (EditText) findViewById(R.id.my_price_input);
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.wishlist_submit_button) {
+            new WishlistAttempt().execute();
+        } else if (v.getId() == R.id.wishlist_cancel_button) {
+            Intent intention = new Intent(this, Homepage.class);
+            finish();
+            startActivity(intention);
         }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -42,19 +57,6 @@ public class Wishlist extends Activity implements View.OnClickListener {
         inflater.inflate(R.menu.action_bar, menu);
         return super.onCreateOptionsMenu(menu);
     }
-
-        @Override
-        public void onClick(View v) {
-            if (v.getId() == R.id.wishlist_cancel_button) {
-                Intent intention = new Intent(this, Homepage.class);
-                finish();
-                startActivity(intention);
-            }
-//            else if (v.getId() == R.id.wishlist_submit_button) {
-//                new Wishlist().addToWishlist();
-//            }
-        }
-
     ArrayList<Item> items;
 
     /**
@@ -67,6 +69,7 @@ public class Wishlist extends Activity implements View.OnClickListener {
     /**
      * Adds an item to this wishlist
      * does not add duplicate items
+     *
      * @param item Item to add to wishlist
      * @return String for success or failure
      */
@@ -81,9 +84,44 @@ public class Wishlist extends Activity implements View.OnClickListener {
 
     /**
      * Returns the backing Arraylist of items in the wishlist
+     *
      * @return Arraylist of Items
      */
     public ArrayList<Item> getWishlist() {
         return items;
     }
+
+    class WishlistAttempt extends AsyncTask<String, String, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(Wishlist.this);
+            progressDialog.setMessage("Submitting...");
+            progressDialog.setIndeterminate(false);
+            progressDialog.setCancelable(true);
+            progressDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... args) {
+            String newItem = enteredItem.getText().toString();
+            String newPrice = enteredPrice.getText().toString();
+            int threshold = Integer.parseInt(newPrice);
+            return DatabaseInterfacer.addToWishlist(newItem, threshold);
+        }
+
+        /**
+         * When a post is executed
+         *
+         * @param file_url
+         */
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog once product deleted
+            progressDialog.dismiss();
+            if (file_url != null) {
+                Toast.makeText(Wishlist.this, file_url, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
 }
